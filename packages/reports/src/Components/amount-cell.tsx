4c4645cd2d3ab1c1e762785d96cace1d520658ff
Aizad Ridzo <aizad@deriv.com>
2023-01-09 12:29:42 +0800

    chore: migrate amount-cell to ts

--- packages/reports/src/Components/amount-cell.tsx
+++ packages/reports/src/Components/amount-cell.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { getProfitOrLoss } from '../Helpers/profit-loss';
 
-const AmountCell = ({ value }) => {
+const AmountCell = (value: string) => {
     const status = getProfitOrLoss(value);
 
     return <span className={`amount--${status}`}>{value}</span>;
-};
-
-AmountCell.propTypes = {
-    value: PropTypes.string,
 };
 
 export default AmountCell;
