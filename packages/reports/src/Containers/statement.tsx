bec5c80108c3fe77eb66026842954d021111a66d
aizad-deriv <aizad@deriv.com>
2023-03-08 16:34:49 +0800

    fix: imported TAccountStatistics inside statement

--- packages/reports/src/Containers/statement.tsx
+++ packages/reports/src/Containers/statement.tsx
@@ -8,7 +8,7 @@ import { getSupportedContracts } from '_common/contract';
 import { connect } from 'Stores/connect';
 import { getStatementTableColumnsTemplate } from '../Constants/data-table-constants';
 import PlaceholderComponent from '../Components/placeholder-component';
-import AccountStatistics from '../Components/account-statistics';
+import AccountStatistics, { TAccountStatistics } from '../Components/account-statistics';
 import FilterComponent from '../Components/filter-component';
 import { ReportsMeta } from '../Components/reports-meta';
 import EmptyTradeHistoryMessage from '../Components/empty-trade-history-message';
